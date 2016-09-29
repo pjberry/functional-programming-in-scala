ca0eccff2a9d6247f825391b7666248f3a634b1c
Patrick Berryhill <pjberry@gmail.com>
2016-09-29 07:31:23 -0500

    Working through first round of stuff

---
+++ src/com/bitmotif/part_1/Exercise_2_1.scala
@@ -0,0 +1,25 @@
+	
+object Exercise_2_1 {
+
+	def fib(n: Int): Int = {
+	  
+	  @annotation.tailrec
+	  def loop(end: Int, loopCount: Int, fibN_Minus1: Int, fibN_Minus2: Int): Int = 
+	    if (end == loopCount) fibN_Minus1 + fibN_Minus2 else loop(end, loopCount + 1, fibN_Minus1 + fibN_Minus2, fibN_Minus1)
+	     
+
+	  if(n < 1) {
+	    throw new IllegalArgumentException("The argument must be 1 or greater.")
+	  }
+	  else if (n == 1)  {
+	   0
+	  }
+	  else if (n == 2) {
+	   1
+	  }
+	  else {
+	   loop(n, 3, 1, 0)
+	  } 
+	} 
+}
+	
