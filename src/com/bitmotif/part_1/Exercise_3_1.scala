05e9b1d3b483c8e38065ecb35522097ccedafa71
Patrick Berryhill <pjberry@gmail.com>
2016-10-09 10:21:34 -0500

    wrapper for the excercise

---
+++ src/com/bitmotif/part_1/Exercise_3_1.scala
@@ -0,0 +1,16 @@
+package com.bitmotif.part_1
+
+object Exercise_3_1 {
+
+  import fpinscala.datastructures._
+  import List._
+
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  } 
+
+}
